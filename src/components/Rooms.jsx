155e04ed415b50a0e3cee9b44cd0996a2bbe970c
Synuhe <synuhe.uribe@gmail.com>
2018-08-10 11:42:08 -0400

    test

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -11,6 +11,7 @@ export default class Rooms extends Component {
         <ul>
           <li>Room 1</li>
           <li>Room 2</li>
+          <li>Room 3</li>
         </ul>
       </React.Fragment>
     )
