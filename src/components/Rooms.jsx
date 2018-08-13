6bff3e54320b4d73d43d482793c7484fdfce8cea
Zach Margolies <zachary.margolies@gmail.com>
2018-08-13 19:35:18 -0400

    premerge

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -1,19 +1,5 @@
 import React, {Component} from 'react'
-<<<<<<< HEAD
-=======
-import db from '../firestore'
-import firebase from 'firebase'
-import Button from '@material-ui/core/Button'
-
-class Rooms extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      rooms: []
-    }
-    this.joinRoom = this.joinRoom.bind(this)
-  }
->>>>>>> master
+import {Link} from 'react-router-dom'
 
 import {withStyles} from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
@@ -26,7 +12,6 @@ import Icon from '@material-ui/core/Icon'
 import DeleteIcon from '@material-ui/icons/Delete'
 import ShareIcon from '@material-ui/icons/Share'
 
-<<<<<<< HEAD
 const styles = theme => ({
   card: {
     maxWidth: 300
@@ -40,24 +25,9 @@ const styles = theme => ({
   },
   button: {
     margin: theme.spacing.unit
-=======
-  async joinRoom(id) {
-    const currentUser = await firebase.auth().currentUser
-    console.log(currentUser.uid)
-
-    await db
-      .collection('users')
-      .doc(currentUser.uid)
-      .update({
-        [id]: true
-      })
-
-    this.props.history.push(`/classroom/${id}`)
->>>>>>> master
   }
 })
 
-<<<<<<< HEAD
 const Rooms = props => {
   const {classes} = props
   return (
@@ -70,14 +40,11 @@ const Rooms = props => {
         />
         <CardContent>
           <Typography gutterBottom variant="headline" component="h2">
-            {props.id}
+            <Link to={`/classroom/${props.id}`}>{props.id}</Link>
           </Typography>
           <Typography component="p">Practice your coding here.</Typography>
         </CardContent>
         <CardActions>
-          {/* <Button size="small" color="primary">
-            Invite
-          </Button> */}
           <Button
             variant="contained"
             color="primary"
@@ -87,9 +54,6 @@ const Rooms = props => {
             {/* <Icon className={classes.rightIcon}>send</Icon> */}
             <ShareIcon className={classes.rightIcon} />
           </Button>
-          {/* <Button size="small" color="primary">
-            Remove
-          </Button> */}
           <Button
             variant="contained"
             color="secondary"
@@ -102,32 +66,6 @@ const Rooms = props => {
       </Card>
     </React.Fragment>
   )
-=======
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.state.rooms.map(room => {
-            return (
-              <div key={this.state.rooms.indexOf(room)}>
-                <li>{room.id}</li>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  onClick={() => {
-                    this.joinRoom(room.id)
-                  }}
-                >
-                  Join Room
-                </Button>
-              </div>
-            )
-          })}
-        </ul>
-      </div>
-    )
-  }
->>>>>>> master
 }
 
 export default withStyles(styles)(Rooms)
