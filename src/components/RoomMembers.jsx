f970c8b710bd51fa4b25b474b4288b59f9b7b301
Zach Margolies <zachary.margolies@gmail.com>
2018-08-16 16:21:33 -0400

    code scrub

--- src/components/RoomMembers.jsx
+++ src/components/RoomMembers.jsx
@@ -3,17 +3,7 @@ import db from '../firestore'
 
 import PropTypes from 'prop-types'
 import {withStyles} from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import AccountCircle from '@material-ui/icons/AccountCircle'
-import Drawer from '@material-ui/core/Drawer'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
 
 const styles = theme => ({
   root: {
--- src/components/RoomStatusBar.jsx
+++ src/components/RoomStatusBar.jsx
@@ -1,7 +1,5 @@
 import React, {Component} from 'react'
-import firebase from 'firebase'
 import db from '../firestore'
-import {render} from 'react-testing-library'
 import RoomMembers from './RoomMembers'
 
 import PropTypes from 'prop-types'
@@ -9,14 +7,6 @@ import {withStyles} from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import AccountCircle from '@material-ui/icons/AccountCircle'
-import Drawer from '@material-ui/core/Drawer'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
 
 const styles = theme => ({
   root: {
@@ -51,16 +41,18 @@ class RoomStatusBar extends Component {
 
   async componentDidMount() {
     if (this.props.roomId) {
-      const room = await db
+      await db
         .collection('rooms')
         .doc(this.props.roomId)
-        .get()
-      const subject = room.data().subject
-      const members = room.data().userIds
-      this.setState({
-        currentRoom: subject,
-        roomMemberIds: members
-      })
+        .onSnapshot(snapshot => {
+          const subject = snapshot.data().subject
+          const members = snapshot.data().userIds
+
+          this.setState({
+            currentRoom: subject,
+            roomMemberIds: members
+          })
+        })
     }
   }
 
