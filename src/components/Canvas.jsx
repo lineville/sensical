53b67613d3e643d84ab6bd80b81a75998ce18679
Zach Margolies <zachary.margolies@gmail.com>
2018-08-19 13:52:07 -0400

    initial commit

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -6,10 +6,7 @@ import {DragSource} from 'react-dnd'
 
 import PropTypes from 'prop-types'
 import {withStyles} from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
+import {Card, CardContent, Button, Typography} from '@material-ui/core/'
 
 const canvasSource = {
   beginDrag(props) {
