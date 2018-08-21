f8b1b39544a1154eb63ffe7496a10df04b32ad31
Synuhe <synuhe.uribe@gmail.com>
2018-08-20 20:08:19 -0400

    test

--- src/components/VideoComponent.jsx
+++ src/components/VideoComponent.jsx
@@ -103,6 +103,8 @@ export class VideoComponent extends Component {
       console.log("Already in Room: '" + participant.identity + "'")
       var previewContainer = this.refs.remoteMedia
       this.attachParticipantTracks(participant, previewContainer)
+      console.log('line106', previewContainer)
+      previewContainer.children[1].setAttribute('width', '100%')
     })
 
     // When a Participant joins the Room, log the event.
