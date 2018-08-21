b5e58d11f24ab13cfa842c4a8eb9451b732592e0
Liam Neville <lineville@vassar.edu>
2018-08-20 21:28:09 -0400

    google button added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -100,6 +100,32 @@ class Login extends Component {
     }
   }
 
+  googleSignup = () => {
+    var provider = new firebase.auth.GoogleAuthProvider()
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken
+        // The signed-in user info.
+        var user = result.user
+
+        this.props.history.push('/profile')
+      })
+      .catch(error => {
+        // Handle Errors here.
+        var errorCode = error.code
+        var errorMessage = error.message
+        // The email of the user's account used.
+        var email = error.email
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential
+        // ...
+      })
+  }
+
   handleLogout = async () => {
     try {
       await firebase.auth().signOut()
@@ -157,6 +183,9 @@ class Login extends Component {
           onClick={this.handleLogin}
         >
           Login
+        </Button>
+        <Button type="submit" id="google" onClick={this.googleSignup}>
+          <img src="/btn_google_signin_light_focus_web@2x.png" id="google" />
         </Button>
 
         <Snackbar
