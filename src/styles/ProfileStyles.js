13651fdb0ec93ba008f576b14d6004b2540cab30
Liam Neville <lneville@quantumscape.com>
2020-05-27 13:09:15 -0700

    smaller profile pic

--- src/styles/ProfileStyles.js
+++ src/styles/ProfileStyles.js
@@ -1,32 +1,32 @@
 import parallaxStyle from './ParallaxStyle'
 
-const styles = theme => ({
+const styles = (theme) => ({
   row: {
-    display: 'flex'
+    display: 'flex',
   },
   avatar: {
-    margin: 50
+    margin: 50,
   },
   bigAvatar: {
-    width: 260,
-    height: 260
+    width: 150,
+    height: 150,
   },
   card: {
-    maxWidth: 345
+    maxWidth: 345,
   },
   margin: {
-    margin: theme.spacing.unit
+    margin: theme.spacing.unit,
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 200
+    width: 200,
   },
   media: {
     height: 0,
-    paddingTop: '56.25%' // 16:9
+    paddingTop: '56.25%', // 16:9
   },
-  ...parallaxStyle
+  ...parallaxStyle,
 })
 
 export default styles
